8cf11078a87280133f97a7e81085a52a5e67781c
farzinmaleknia <farzin.en@gmail.com>
2023-08-12 17:02:21 +0330

    Zoom and Move the div Completed

--- DirectedGraph/wwwroot/content/script/js-cust.js
+++ DirectedGraph/wwwroot/content/script/js-cust.js
@@ -17,3 +17,52 @@ function getElementDimension(id) {
     }
 
 }
+
+function makeDivZoomable(id) {
+
+    var isDown = false;
+    var zoomable = document.getElementById(`${id}`); //.getElementsByTagName('p')
+    zX = 1;
+
+    console.log( "befor" , zoomable.getBoundingClientRect().width)
+    window.addEventListener('wheel', function (e) {
+        var dir;
+        dir = (e.deltaY > 0) ? -0.1 : 0.1;
+        zX += dir;
+
+        zoomable.style.transform = 'scale(' + zX + ')';
+
+        e.preventDefault();
+        return;
+    }, { passive: false });
+
+
+    zoomable.addEventListener('mousedown', function (e) {
+        isDown = true;
+        offset = [
+            zoomable.offsetLeft - e.clientX,
+            zoomable.offsetTop - e.clientY
+        ];
+        zoomable.style.cursor = "grabbing";
+    }, true);
+
+    document.addEventListener('mouseup', function () {
+        isDown = false;
+        zoomable.style.cursor = "pointer";
+    }, true);
+
+    document.addEventListener('mousemove', function (event) {
+        event.preventDefault();
+        if (isDown) {
+            mousePosition = {
+
+                x: event.clientX,
+                y: event.clientY
+
+            };
+            zoomable.style.left = (mousePosition.x + offset[0]) + 'px';
+            zoomable.style.top = (mousePosition.y + offset[1]) + 'px';
+        }
+    }, true);
+
+}
